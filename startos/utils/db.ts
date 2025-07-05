0187037c20e753bb789c63b2e7e309f81cd0a5b6
aldum <aldum@artixlinux.org>
2025-07-05 22:20:59 +0200

    refactor: split utils

---
+++ startos/utils/db.ts
@@ -0,0 +1,6 @@
+import postgres from 'postgres'
+
+export const psqlDaemonUser = "postgres"
+export const psqlHost = "localhost"
+export const psqlUser = "postgres"
+export const psqlPort = 5432
---
+++ startos/utils/index.ts
@@ -0,0 +1,5 @@
+export const uiPort = 8080
+export const davPort = 5232
+
+export * from './db'
+export * from './password'
