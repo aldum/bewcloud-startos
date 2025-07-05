f2cbd39057f2b8169cb70f70150d812a8d956d91
aldum <aldum@artixlinux.org>
2025-07-05 22:20:59 +0200

    DB utils

--- startos/utils/db.ts
+++ startos/utils/db.ts
@@ -1,4 +1,5 @@
 import postgres from 'postgres'
+import { storeJson } from '../fileModels/store.json'
 
 export const psqlDaemonUser = "postgres"
 export const psqlHost = "localhost"
@@ -6,5 +7,60 @@ export const psqlUser = "postgres"
 export const psqlPort = 5432
 
 export class DB {
-  static getAdminUserId = () => { }
+  private static sql = postgres({
+    host: psqlHost,
+    port: psqlPort,
+    user: psqlUser,
+    db: psqlUser,
+    pass: ""
+  })
+
+  private static usersTable = this.sql`bewcloud_users`
+
+  static getUsers = async () => {
+    return this.sql`
+      SELECT *
+      FROM ${this.usersTable}
+      `
+  }
+
+  static getAdminUserId = async (): Promise<string> => {
+    return this.sql`
+      SELECT id
+      FROM ${this.usersTable}
+      WHERE extra->>'is_admin' = 'true'
+      `.values().then(res => res[0][0] as string)
+  }
+
+  static createUser =
+    async (
+      email: string, pwHash: string
+    ): Promise<string> => {
+      const em = this.sql`${email}`
+      const pw = this.sql`${pwHash}`
+
+      return this.sql`
+      INSERT INTO ${this.usersTable} (email, hashed_password, extra)
+      VALUES (${em}, ${pw}, '{ "isAdmin": true }')
+      RETURNING id
+      `.values().then(res => res[0][0] as string)
+    }
+
+  static updateUserPassword =
+    async (
+      uuid: string, pwHash: string
+    ) => {
+      const id = this.sql`${uuid}`
+      const pw = this.sql`${pwHash}`
+
+      return this.sql`
+      UPDATE ${this.usersTable}
+      SET hashed_password = ${pw}
+      WHERE id = ${id}
+      `
+    }
+
+  static helth = async () => {
+    return this.sql`SELECT 1`.simple()
+  }
 }
